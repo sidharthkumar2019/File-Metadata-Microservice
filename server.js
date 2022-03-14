b5175bdc9e31b8725da7fd0533aa7a38da40edba
sidharthkumar2k19 <sidharthkumar2k19@gmail.com>
2022-03-14 23:50:40 +0530

    completed

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
@@ -11,8 +13,10 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), async(req, res) => {
+  const {originalname, mimetype, size} = req.file;
+  res.send({name: originalname, type: mimetype, size: size});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
